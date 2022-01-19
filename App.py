c76a154ca2fc8f6b2f2ea739cd4bcc7d92659488
Kenneth Njuguna <njuguna.kenneth@outlook.com>
2022-01-19 10:09:09 +0300

    Python Hello World

--- App.py
+++ App.py
@@ -1,4 +1,9 @@
 age=30
 age= 124
+Price=98.5450
+first_name="Alice"
+is_offline= True
 print('Hello world Kenneth Njuguna First Python Program')
 print(age)
+
+
---
+++ exercise.py
@@ -0,0 +1,6 @@
+first_name="John Smith"
+age=(20)
+newpatient=True
+print(first_name)
+print(age)
+print(newpatient)
