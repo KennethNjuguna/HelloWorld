a6a970261d57d46677b54b9cd3bf15d87085950b
Kenneth Njuguna <njuguna.kenneth@outlook.com>
2022-01-19 11:29:04 +0300

    Python Hello World

---
+++ conversions.py
@@ -0,0 +1,4 @@
+Birth_year= input("What is your Birth year: ")
+age = 2021 - int(Birth_year)
+print ("Age is: " + str(age))
+
---
+++ sum.py
@@ -0,0 +1,5 @@
+first_number= input("First Number: ")
+second_number= input("second_number: ")
+sum= float(first_number) + float(second_number)
+
+print("Sum is: "+ str(sum))
