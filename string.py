68db33b3a018f8da39ffa8311719ecfc8136db2e
Kenneth Njuguna <njuguna.kenneth@outlook.com>
2022-01-19 14:57:05 +0300

    Python Hello World

--- string.py
+++ string.py
@@ -1,2 +1,5 @@
-course= "Python for beginners"
+course= "Kenneth Njuguna is now learning software development"
 print(course.replace("for","4"))
+print(course.upper())
+print('software' in course)
+print(course)
