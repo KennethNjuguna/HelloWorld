2e64eee2f10602726123df4b602806beaab9a09a
Kenneth Njuguna <njuguna.kenneth@outlook.com>
2022-01-20 09:54:28 +0300

    Python Hello World

--- ifoperators.py
+++ ifoperators.py
@@ -1,3 +1,8 @@
-temperature= 35.6
-if temperature>35.6:
+temperature= 35
+if temperature>40:
     print("it's a very hot day")
+    print("drink lots of water")
+elif temperature<45:
+    print("it is a cold day")
+    print("please dress warmly")
+
