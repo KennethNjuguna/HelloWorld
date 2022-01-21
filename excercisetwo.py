3c65fbb5ecf5b1d01289835b5ef3a35d70967fb9
Kenneth Njuguna <njuguna.kenneth@outlook.com>
2022-01-21 16:38:44 +0300

    Python Hello World

--- excercisetwo.py
+++ excercisetwo.py
@@ -1,13 +1,15 @@
-weight= int(input("weight: "))
+weight= float(input("weight: "))
 unit=(input("(K)g or (L)bs: "))
 
 
-if unit == "l":
+if unit == "k":
+    weight_k = weight / 2.20462
+    print("weight in Pounds: " + str(weight_k))
+
+elif unit == "l":
+
     weight_l = weight * 2.20462
     print("weight in Kg: " + str(weight_l))
-elif unit == "k":
-    weight_k=weight / 2.20462
-    print("weight in Pounds: " + str(weight_k))
 print("done")
 
 
