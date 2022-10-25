a1a997f25221fa6eada9bb40a05978acae27ba0d
Kenneth Njuguna <njuguna.kenneth@outlook.com>
2022-10-25 14:37:13 +0300

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,5 +1,5 @@
 first_number= input("First Number: ")
-second_number= input("second_number: ")
-sum= float(first_number) + float(second_number)
+2nd_number= input("second_number: ")
+sum= float(first_number) + float(2nd_number)
 
 print("Sum is: "+ str(sum))
