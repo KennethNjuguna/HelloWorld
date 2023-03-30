eafd681f5ea9fd9973c0c62beb2fa2d0b0719bcf
Kenneth Njuguna <njuguna.kenneth@outlook.com>
2023-03-30 10:29:37 +0300

    Exercise two commit

--- excercisetwo.py
+++ excercisetwo.py
@@ -4,10 +4,11 @@ weight= float(input("weight: "))
 unit=(input("(K)g or (L)bs: "))
 
 #Below code converts Weight declared in (K)g to (L)bs
+#This code implements if to change the flow
 if unit == "k":
     weight_k = weight / 2.20462
     print("weight in Pounds: " + str(weight_k))
-
+#"elif" means else if to give the other scenario if a user inputs (L)bs to convert to (K)g
 elif unit == "l":
 
     weight_l = weight * 2.20462
